adc40a7ac12432dd8d58671f8db3b53ffa8c4d07
wanderok <wanderok@msn.com>
2020-10-18 20:25:23 -0300

    Casa para Geovana

--- Enviar.bat
+++ Enviar.bat
@@ -1,5 +1,5 @@
 C:
-XCOPY C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\built\SMC_SISTEMA\SMC_PLUS*.* C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\src\PROJETO
+XCOPY C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\built\SMC_SISTEMA\SMC_PLUS*.* C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\src\PROJETO /S /Y /I /D
 xcopy C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\src C:\WANDER\OK\GEOVANA_OK\SRC /S /Y /I /D
 CD \WANDER\OK\GEOVANA_OK
 git status
